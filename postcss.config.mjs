b94d1254cf5644506b8bc6e9cfbaf308e1943e1c
BroYears <micael456@naver.com>
2025-08-12 11:09:06 +0900

    버그 수정

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+import tailwindcss from '@tailwindcss/postcss'; // 새로 설치한 패키지를 import
+import autoprefixer from 'autoprefixer';
+
+export default {
+    plugins: [
+        tailwindcss,
+        autoprefixer,
+    ],
 };
 
 export default config;
